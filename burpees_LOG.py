965b70c0fdd31a723ba1c0ad4c18c226eef085db
majkel44 <mikemikhael@duck.com>
2024-05-24 09:12:49 +0200

    Updated the base DataFrame

--- burpees_LOG.py
+++ burpees_LOG.py
@@ -6,4 +6,8 @@ import numpy as np
 columns_names = ['TotalBurpees', 'BurpeesVariation', 'TotalPush-ups', 'TotalTime']
 burpees_log = pd.DataFrame(index=pd.date_range('2024-05-20', end='2024-05-26', freq='D'), columns=columns_names)
 
+burpees_log['TotalBurpees'] = [100, 300, 100, 300, 100, 0, 0]
+burpees_log['BurpeesVariation'] = ['6CT', 'NS', '6CT', 'NS', '6CT', 'NS', 'NS']
+burpees_log['TotalPush-ups'] = [100, 100, 100, 100, 100, 100, 100]
+
 print(burpees_log)
